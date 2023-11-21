6a9aba6714989df06e2fa67d222b0401e5ef6a7d
Sami Ekblad <sami@vaadin.com>
2023-11-21 13:43:19 +0200

    Added tests

---
+++ src/test/java/org/parttio/vaadinjsloader/MainIT.java
@@ -0,0 +1,46 @@
+package org.parttio.vaadinjsloader;
+
+import com.microsoft.playwright.Browser;
+import com.microsoft.playwright.BrowserType;
+import com.microsoft.playwright.Page;
+import com.microsoft.playwright.Playwright;
+import org.junit.jupiter.api.Test;
+
+import static com.microsoft.playwright.assertions.PlaywrightAssertions.assertThat;
+
+public class MainIT {
+
+    static Playwright playwright = Playwright.create(); // <4>
+    private String url = "http://localhost:8099/test";
+    private BrowserType.LaunchOptions options = new BrowserType.LaunchOptions().setHeadless(true);
+
+    @Test
+    public void cdnjsLoads() {
+        try (Browser browser = playwright.chromium().launch(options)) { // <5>
+            Page page = browser.newPage();
+            page.navigate(url);
+            page.getByText("Load from cdnjs.com").first().click();
+            assertThat(page.getByText("loaded https://cdnjs")).isVisible(); // <8>
+        }
+    }
+
+    @Test
+    public void unpkgLoads() {
+        try (Browser browser = playwright.chromium().launch(options)) { // <5>
+            Page page = browser.newPage();
+            page.navigate(url);
+            page.getByText("Load from unpkg.com").first().click();
+            assertThat(page.getByText("loaded https://unpkg.com/")).isVisible(); // <8>
+        }
+    }
+
+    @Test
+    public void loacalLoads() {
+        try (Browser browser = playwright.chromium().launch(options)) { // <5>
+            Page page = browser.newPage();
+            page.navigate(url);
+            page.getByText("Load local").first().click();
+            assertThat(page.getByText("loaded http://localhost")).isVisible(); // <8>
+        }
+    }
+}
---
+++ src/test/java/org/parttio/vaadinjsloader/TestView.java
@@ -0,0 +1,63 @@
+package org.parttio.vaadinjsloader;
+
+import com.vaadin.flow.component.ClickEvent;
+import com.vaadin.flow.component.UI;
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.html.H1;
+import com.vaadin.flow.component.html.Paragraph;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.router.Route;
+import com.vaadin.flow.theme.Theme;
+import com.vaadin.flow.theme.material.Material;
+
+@Route
+public class TestView extends VerticalLayout {
+
+    public TestView() {
+        add(new H1("Addon test"));
+        add(new Button("Load from cdnjs.com", this::loadLibraryCdnJs));
+        add(new Button("Load from unpkg.com", this::loadLibraryUnpkg));
+        add(new Button("Load local", this::loadLibraryCustom));
+    }
+
+    private void loadLibraryCdnJs(ClickEvent<Button> buttonClickEvent) {
+        // Load some script
+        String library = "jquery";
+        String version = "3.7.1";
+        JSLoader.loadCdnjs(this, library, version);
+
+        // Check that the script is indeed loaded
+        UI.getCurrent()
+                .getPage()
+                .executeJs("return document.querySelector('script[src*=\"" + library + "\"]').src")
+                .then(src -> add(new Paragraph("loaded "+src.asString())));
+    }
+
+    private void loadLibraryUnpkg(ClickEvent<Button> buttonClickEvent) {
+        // Load some script
+        String library = "three";
+        String version = "0.158.0";
+        JSLoader.loadUnpkg(this, library, version);
+
+        // Check that the script is indeed loaded
+        UI.getCurrent()
+                .getPage()
+                .executeJs("return document.querySelector('script[src*=\"" + library + "\"]').src")
+                .then(src -> add(new Paragraph("script loaded "+src.asString())));
+    }
+
+    private void loadLibraryCustom(ClickEvent<Button> buttonClickEvent) {
+        // Load some script
+        String library = "mylib";
+        String version = "1.0";
+        String file = "mylib.js";
+        String urlPattern = "/{library}-{version}/{file}";
+        JSLoader.load(this, library, version, file, urlPattern);
+
+        // Check that the script is indeed loaded
+        UI.getCurrent()
+                .getPage()
+                .executeJs("return document.querySelector('script[src*=\"" + library + "\"]').src")
+                .then(src -> add(new Paragraph("script loaded "+src.asString())));
+    }
+}
